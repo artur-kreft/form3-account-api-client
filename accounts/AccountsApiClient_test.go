f42c91a3381069200b8aa65f04f74bf2a2dd4837
Artur Kreft <artur.kreft@bytedance.com>
2022-05-16 13:56:02 +0200

    added happy path tests for create

--- accounts/AccountsApiClient_test.go
+++ accounts/AccountsApiClient_test.go
@@ -40,96 +40,77 @@ func TestAccountsClient_GetShouldFailForMissingAccount(t *testing.T) {
 func TestAccountsClient_GetShouldReturnCorrectAttributes(t *testing.T) {
 	client := NewClient(*apiUrlFlag)
 
-	organisation_id := uuid.NewV1()
-	name := []string{"a"}
-	bank_id := "400300"
-	bank_id_code := "GBDSC"
-	bic := "NWBKGB22"
-	joint := false
-	matching := false
-	secondary_id := "A1B2C3D4"
-	country := countries.ByName("PL")
-	ccy := countries.CurrencyAED
-	classification := Business
-	status := Pending
-	alternative := []string{"a", "a", "a"}
-	switched := true
-	attributes := &AccountAttributes{
-		Name:                    name,
-		Country:                 &country,
-		BaseCurrency:            &ccy,
-		AccountClassification:   &classification,
-		BankID:                  bank_id,
-		BankIDCode:              bank_id_code,
-		Bic:                     bic,
-		SecondaryIdentification: secondary_id,
-		JointAccount:            &joint,
-		AccountMatchingOptOut:   &matching,
-		AccountNumber:           "GB11NWBK40030041426819",
-		Iban:                    "GB11NWBK40030041426819",
-		AlternativeNames:        alternative,
-		Status:                  &status,
-		Switched:                &switched,
+	organisationId := []uuid.UUID{uuid.NewV1(), uuid.NewV1()}
+	name := [][]string{{"a"}, {"Jan", "Kowalski"}}
+	bankId := []string{"400300", "5696993"}
+	bankIdCode := []string{"GBDSC", "PLOIHFD"}
+	bic := []string{"NWBKGB22", "KJILDJ99"}
+	joint := []bool{false, true}
+	matching := []bool{false, true}
+	accountNumber := []string{"40030041426819", "SKJINJKS98854"}
+	iban := []string{"GB11NWBK40030041426819", "PL99999999999999999999"}
+	secondaryId := []string{"A1B2C3D4", "34GJ98FD"}
+	country := []countries.CountryCode{countries.ByName("PL"), countries.ByName("DE")}
+	ccy := []countries.CurrencyCode{countries.CurrencyAED, countries.CurrencyBIF}
+	classification := []EnumClassification{Business, Personal}
+	status := []EnumStatus{Pending, Confirmed}
+	alternative := [][]string{{"a", "a", "a"}, {"Mike", "Jordan"}}
+	switched := []bool{false, true}
+	attributes := [2]AccountAttributes{}
+
+	for i := 0; i < len(attributes); i++ {
+		attributes[i] = AccountAttributes{
+			Name:                    name[i],
+			Country:                 &country[i],
+			BaseCurrency:            &ccy[i],
+			AccountClassification:   &classification[i],
+			BankID:                  bankId[i],
+			BankIDCode:              bankIdCode[i],
+			Bic:                     bic[i],
+			SecondaryIdentification: secondaryId[i],
+			JointAccount:            &joint[i],
+			AccountMatchingOptOut:   &matching[i],
+			AccountNumber:           accountNumber[i],
+			Iban:                    iban[i],
+			AlternativeNames:        alternative[i],
+			Status:                  &status[i],
+			Switched:                &switched[i],
+		}
 	}
 
-	resCreate, errCreate := client.CreateAccount(context.Background(), &organisation_id, attributes)
-	defer cleanup(t, client, resCreate)
+	tests := []struct {
+		name           string
+		organisationId *uuid.UUID
+		attributes     *AccountAttributes
+	}{
+		{
+			name:           "Returned attibutes should be equal",
+			organisationId: &organisationId[0],
+			attributes:     &attributes[0],
+		},
+		{
+			name:           "Returned attibutes should be equal",
+			organisationId: &organisationId[1],
+			attributes:     &attributes[1],
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resCreate, errCreate := client.CreateAccount(context.Background(), tt.organisationId, tt.attributes)
+			defer cleanup(t, client, resCreate)
 
-	assert.NoError(t, errCreate)
+			assert.NoError(t, errCreate)
 
-	resGet, errGet := client.GetAccount(context.Background(), resCreate.ID)
-	assert.NotNil(t, resGet)
-	assert.NoError(t, errGet)
-	assert.EqualValues(t, resCreate.ID, resGet.ID)
-	assert.EqualValues(t, attributes, resGet.Attributes)
-	assert.EqualValues(t, &organisation_id, resGet.OrganisationID)
-	assert.EqualValues(t, 0, *resGet.Version)
-}
-
-func TestAccountsClient_CreateShouldReturnCorrectAttributes(t *testing.T) {
-
-	client := NewClient(*apiUrlFlag)
-
-	organisation_id := uuid.NewV1()
-	name := []string{"a"}
-	bank_id := "400300"
-	bank_id_code := "GBDSC"
-	bic := "NWBKGB22"
-	joint := false
-	matching := false
-	secondary_id := "A1B2C3D4"
-	country := countries.ByName("PL")
-	ccy := countries.CurrencyAED
-	classification := Business
-	status := Pending
-	alternative := []string{"a", "a", "a"}
-	switched := true
-	attributes := &AccountAttributes{
-		Name:                    name,
-		Country:                 &country,
-		BaseCurrency:            &ccy,
-		AccountClassification:   &classification,
-		BankID:                  bank_id,
-		BankIDCode:              bank_id_code,
-		Bic:                     bic,
-		SecondaryIdentification: secondary_id,
-		JointAccount:            &joint,
-		AccountMatchingOptOut:   &matching,
-		AccountNumber:           "GB11NWBK40030041426819",
-		Iban:                    "GB11NWBK40030041426819",
-		AlternativeNames:        alternative,
-		Status:                  &status,
-		Switched:                &switched,
+			resGet, errGet := client.GetAccount(context.Background(), resCreate.ID)
+			assert.NotNil(t, resGet)
+			assert.NoError(t, errGet)
+			assert.EqualValues(t, resCreate.ID, resGet.ID)
+			assert.EqualValues(t, tt.attributes, resGet.Attributes)
+			assert.EqualValues(t, tt.organisationId, resGet.OrganisationID)
+			assert.EqualValues(t, 0, *resGet.Version)
+		})
 	}
 
-	res, err := client.CreateAccount(context.Background(), &organisation_id, attributes)
-	defer cleanup(t, client, res)
-
-	assert.NotNil(t, res)
-	assert.Nil(t, err)
-	assert.EqualValues(t, attributes, res.Attributes)
-	assert.EqualValues(t, &organisation_id, res.OrganisationID)
-	assert.EqualValues(t, 0, *res.Version)
 }
 
 func TestAccountsApiClient_CreateAccountWithWrongInput(t *testing.T) {
@@ -186,6 +167,12 @@ func TestAccountsApiClient_CreateAccountWithWrongInput(t *testing.T) {
 			attributes:    &AccountAttributes{Name: []string{"a"}, Country: &country, Bic: "nwbkgb22"},
 			expectedError: "bic in body should match '^([A-Z]{6}[A-Z0-9]{2}|[A-Z]{6}[A-Z0-9]{5})$'",
 		},
+
+		{
+			name:          "Should fail when BIC has incorrect format",
+			attributes:    &AccountAttributes{Name: []string{"a"}, Country: &country, Bic: "0WBKGB22"},
+			expectedError: "bic in body should match '^([A-Z]{6}[A-Z0-9]{2}|[A-Z]{6}[A-Z0-9]{5})$'",
+		},
 		{
 			name:          "Should fail when BIC has incorrect format",
 			attributes:    &AccountAttributes{Name: []string{"a"}, Country: &country, Bic: "jlkjiogfdhgsd"},
@@ -208,12 +195,12 @@ func TestAccountsApiClient_CreateAccountWithWrongInput(t *testing.T) {
 		},
 		{
 			name:          "Should fail when BankID has incorrect format",
-			attributes:    &AccountAttributes{Name: []string{"a"}, Country: &country, BankID: "gdgrygfjgf"},
+			attributes:    &AccountAttributes{Name: []string{"a"}, Country: &country, BankID: "AAAAAAAA1AAAAAA1A"},
 			expectedError: "bank_id in body should match '^[A-Z0-9]{0,16}$'",
 		},
 		{
 			name:          "Should fail when BankIDCode has incorrect format",
-			attributes:    &AccountAttributes{Name: []string{"a"}, Country: &country, BankIDCode: "gdgrygfjgf"},
+			attributes:    &AccountAttributes{Name: []string{"a"}, Country: &country, BankIDCode: "AAAAAAAA1"},
 			expectedError: "bank_id_code in body should match '^[A-Z]{0,16}$'",
 		},
 		{
